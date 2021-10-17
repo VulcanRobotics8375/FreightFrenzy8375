19b1e813319849f9a375c0afa1ce6b540aa26e7f
Mathew Zheng <mathewzheng9999@gmail.com>
2021-10-17 16:00:49 -0700

    new PID and first PID test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotcorelib/math/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotcorelib/math/PID.java
@@ -1,5 +1,28 @@
 package org.firstinspires.ftc.teamcode.robotcorelib.math;
 
+import java.util.*;
+import java.io.*;
+
 public class PID {
-    //for matt to fill out
+    private double Kp;
+    private double Ki;
+    private double Kd;
+    private double error;
+    private double lastError;
+    private double integral;
+    private double derivative;
+
+    public PID(double Kp, double Ki, double Kd){
+        this.Kp = Kp;
+        this.Ki = Ki;
+        this.Kd = Kd;
+    }
+
+    public double run(double target, double measurement){
+        error = target - measurement;
+        integral += (error + lastError) / 2.0;
+        derivative = error - lastError;
+        lastError = error;
+        return Kp * error + Ki * integral + Kd * derivative;
+    }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/PIDTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.robotcorelib.math.PID;
+import org.firstinspires.ftc.teamcode.robotcorelib.opmode.OpModePipeline;
+import org.firstinspires.ftc.teamcode.robotcorelib.robot.Robot;
+import org.firstinspires.ftc.teamcode.robotcorelib.util.RobotRunMode;
+
+@TeleOp
+public class PIDTest extends OpModePipeline {
+    private DcMotor motor;
+    PID pid = new PID(0.01, 0, 0);
+    public void init(){
+        runMode = RobotRunMode.TELEOP;
+        super.init();
+        motor = hardwareMap.dcMotor.get("motor");
+    }
+
+    @Override
+    public void loop() {
+        Robot.update();
+        double output = pid.run(200, motor.getCurrentPosition());
+        motor.setPower(output);
+    }
+}
