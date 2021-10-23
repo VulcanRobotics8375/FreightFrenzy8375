604f39dd8736117f78865952f2d5a94eb87b50d7
nicobonik <nicobonik.awesome@gmail.com>
2021-10-23 16:29:55 -0700

    more work on motion profiling

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotcorelib/motion/TrapezoidalMotionProfile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotcorelib/motion/TrapezoidalMotionProfile.java
@@ -5,6 +5,7 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 public class TrapezoidalMotionProfile extends BasicMotionProfile {
 
     //TODO all of this lmfao idk what i am doing
+    //TODO get rid of pose2d, should just be scalars for distance for basic shit
 
     public TrapezoidalMotionProfile(double maxVel, double maxAccel, Pose2d start, Pose2d end) {
         super(maxVel, maxAccel, start, end);
