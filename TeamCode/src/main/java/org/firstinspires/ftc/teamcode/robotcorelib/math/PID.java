73f3194d56593abc807f0fc237f18fc6fd3f21ec
Mathew Zheng <mathewzheng9999@gmail.com>
2021-10-23 16:01:23 -0700

    PID Integral Clamping V1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotcorelib/math/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotcorelib/math/PID.java
@@ -7,15 +7,23 @@ public class PID {
     private double Kp;
     private double Ki;
     private double Kd;
+    private double outputMaxLimit;
+    private double outputMinLimit;
     private double error;
     private double lastError;
     private double integral;
     private double derivative;
+    private double controllerOutput;
+    private double controllerOutputB;
+    private boolean isSaturating = false;
+    private boolean integratorSaturate = false;
 
-    public PID(double Kp, double Ki, double Kd){
+    public PID(double Kp, double Ki, double Kd, double outputMaxLimit, double outputMinLimit){
         this.Kp = Kp;
         this.Ki = Ki;
         this.Kd = Kd;
+        this.outputMaxLimit = outputMaxLimit;
+        this.outputMinLimit = outputMinLimit;
     }
 
     public double run(double target, double measurement){
@@ -23,6 +31,31 @@ public class PID {
         integral += (error + lastError) / 2.0;
         derivative = error - lastError;
         lastError = error;
-        return Kp * error + Ki * integral + Kd * derivative;
+
+        // Matt Attempt at Integral AntiWindup: Clamping
+        controllerOutput = Kp * error + Ki * integral + Kd * derivative;
+        controllerOutputB = controllerOutput;
+        if(controllerOutput > outputMaxLimit){
+            controllerOutput = outputMaxLimit;
+        } else if(controllerOutput < outputMinLimit){
+            controllerOutput = outputMinLimit;
+        }
+
+        if(controllerOutputB == controllerOutput){
+            isSaturating = false;
+        }else{
+            isSaturating = true;
+        }
+
+        if((controllerOutputB > 0 && error > 0) || (controllerOutputB < 0 && error < 0)){
+            integratorSaturate = true;
+        } else{
+            integratorSaturate = false;
+        }
+
+        if(isSaturating && integratorSaturate){
+            controllerOutput = Kp * error + Kd * derivative;
+        }
+        return controllerOutput;
     }
 }
