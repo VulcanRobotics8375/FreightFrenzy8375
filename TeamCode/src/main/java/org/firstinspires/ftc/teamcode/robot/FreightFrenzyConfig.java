5a4b3aa811c03f4478d45b4105803fa97da4e643
nicobonik <nicobonik.awesome@gmail.com>
2021-11-11 22:35:02 -0500

    add lift to ff config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/FreightFrenzyConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/FreightFrenzyConfig.java
@@ -3,16 +3,19 @@ package org.firstinspires.ftc.teamcode.robot;
 import org.firstinspires.ftc.teamcode.robotcorelib.robot.RobotConfig;
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.Lift;
 
 public class FreightFrenzyConfig extends RobotConfig {
 
     public Drivetrain drivetrain;
     public Intake intake;
+    public Lift lift;
 
     @Override
     public void init() {
         subsystems.clear();
         drivetrain = new Drivetrain();
         intake = new Intake();
+        lift = new Lift();
     }
 }
