f403d11337d263be9b7770ead0bdf1773243317f
RohilKalra <68201571+RohilKalra@users.noreply.github.com>
2021-11-21 16:52:26 -0800

    Updated Cap to slow it down

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Cap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Cap.java
@@ -2,13 +2,18 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.teamcode.robotcorelib.util.Subsystem;
+// import java.time.*;
 
 public class Cap extends Subsystem {
     private Servo arm;
-    private double down = 0.55;
-    private double up = 0.01;
+
+    private double down = 0.6;
+    private double up = 0.02;
     private double open = -1;
     private boolean changeOpen = false;
+
+    private double position = up;
+    private double increment = 0.005;
 
     @Override
     public void init() {
@@ -25,10 +30,19 @@ public class Cap extends Subsystem {
            this.changeOpen = false;
        }
        if(open > 0){
-           arm.setPosition(down);
+           // updated to slow down so cap doesn't fly out
+           if(position + increment <= 0.6) {
+               arm.setPosition(position+increment);
+               position += increment;
+           } else if(position + increment > 0.6) {
+               arm.setPosition(down);
+               position = 0.6;
+           }
+
        }
        if(open < 0){
            arm.setPosition(up);
+           position = up;
        }
     }
 
