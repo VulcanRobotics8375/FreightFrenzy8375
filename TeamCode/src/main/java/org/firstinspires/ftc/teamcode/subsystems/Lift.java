d03cc0637fcf2db72d850f1a4688d75eb2afb6eb
Mathew Zheng <mathewzheng9999@gmail.com>
2022-02-13 18:05:05 -0800

    Auto Aim Turret && Unfinished Buttons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.robotcorelib.math.PID;
+import org.firstinspires.ftc.teamcode.robotcorelib.math.SimplePID;
+import org.firstinspires.ftc.teamcode.robotcorelib.robot.Robot;
+import org.firstinspires.ftc.teamcode.robotcorelib.util.Point;
 import org.firstinspires.ftc.teamcode.robotcorelib.util.Subsystem;
 
 public class Lift extends Subsystem {
@@ -14,12 +18,23 @@ public class Lift extends Subsystem {
     private Servo release;
     private Servo linkage;
 
-    PID pid = new PID(0.0005, 0, 0, 1, -1);
+    SimplePID pid = new SimplePID(0.0005, 0, 0, 1, -1);
+    SimplePID turnPID = new SimplePID(0.05,0,0,1,-1);
 
     private double releaseOpen = -1;
     private boolean releaseButton = false;
     private double releasePosOpen = 0.5;
     private double releasePosClosed = 0.05;
+    //Temporary until analog input
+    private AnalogInput turretAngleAnalog;
+
+    private boolean autoAim = false;
+
+    public final double GOAL_X = 0;
+    public final double GOAL_Y = 0;
+    public final int BASE_TURRET_POS = 0;
+    public final int BASE_LIFT_POS = 0;
+    public final double BASE_LINKAGE_POS = 0.01;
 
     public void init(){
         release = hardwareMap.servo.get("release");
@@ -28,10 +43,35 @@ public class Lift extends Subsystem {
         turret = hardwareMap.dcMotor.get("turret");;
         lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        turret.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        turret.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         lift.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
-    public void run(double liftStick, double turretStick, boolean releaseButton) {
+    public void run(double liftStick, double turretStick, boolean turretButton, boolean resetButton, boolean releaseButton) {
+
+        //Auto-aim for auto
+        Point target = new Point(GOAL_X, GOAL_Y);
+        double turretAngle = turretAngleAnalog.getVoltage();
+        if(autoAim){
+            double angleToTarget = Math.atan2(target.x - Robot.getRobotPose().getX(), target.y - Robot.getRobotPose().getY());
+            double currentAngle = Robot.getRobotPose().getHeading() + turretAngle;
+            turret.setPower(turnPID.run(angleToTarget, currentAngle));
+        }
+
+        //Boooooooooooootons.
+        if(resetButton){
+            linkage.setPosition(BASE_LINKAGE_POS);
+            liftToPosition(BASE_LIFT_POS);
+            turretToPosition(BASE_TURRET_POS);
+            
+        }
+
+        if(Math.abs(turretStick) > 0.05){
+            turret.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            turret.setPower(turretStick);
+        }
+
 
         //Hopper Code
         if(releaseButton && !this.releaseButton) {
@@ -48,6 +88,18 @@ public class Lift extends Subsystem {
             release.setPosition(releasePosClosed);
         }
 
+    }
+
+    public void turretToPosition(int pos){
+        turret.setTargetPosition(pos);
+        turret.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        turret.setPower(1);
+    }
+
+    public void liftToPosition(int pos) {
+        lift.setTargetPosition(pos);
+        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lift.setPower(1);
     }
 
     public void test(double liftStick) {
