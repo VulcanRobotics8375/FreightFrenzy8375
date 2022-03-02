6f137638ebe42980ed93d69a6898eb92c488d8a7
Nico Paoli <39326876+nicobonik@users.noreply.github.com>
2022-03-02 11:08:39 -0800

    (robotcorelib) add support for 1D kinematic models with inverse kinematic model calculation

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotcorelib/motion/kinematics/AbstractModel1D.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.robotcorelib.motion.kinematics;
+
+import org.apache.commons.math3.analysis.UnivariateFunction;
+import org.apache.commons.math3.analysis.solvers.BisectionSolver;
+
+public class AbstractModel1D {
+
+    private UnivariateFunction function;
+    private double min, max;
+    private BisectionSolver solver = new BisectionSolver();
+
+    public AbstractModel1D(UnivariateFunction function, double min, double max) {
+        this.function = function;
+        this.min = min;
+        this.max = max;
+    }
+
+    public double inverse(double y) {
+        return solver.solve(1000, new InverseUnivariateFunctionSolver(function, y), min, max);
+    }
+
+    public double value(double x) {
+        return function.value(x);
+    }
+
+    static class InverseUnivariateFunctionSolver implements UnivariateFunction {
+        private final UnivariateFunction function;
+        private final double inverseValue;
+
+        public InverseUnivariateFunctionSolver(UnivariateFunction function, double inverseValue) {
+            this.function = function;
+            this.inverseValue = inverseValue;
+        }
+
+        @Override
+        public double value(double x) {
+            return function.value(x) - inverseValue;
+        }
+    }
+
+}
