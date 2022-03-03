8fa55658525588db935c95deee8a9a3a34aa163b
Nico Paoli <39326876+nicobonik@users.noreply.github.com>
2022-03-03 12:30:37 -0800

    label lift run method buttons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Main.java
@@ -1,11 +1,7 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.teamcode.robot.FreightFrenzyConfig;
 import org.firstinspires.ftc.teamcode.robotcorelib.opmode.OpModePipeline;
 import org.firstinspires.ftc.teamcode.robotcorelib.robot.Robot;
@@ -32,9 +28,20 @@ public class Main extends OpModePipeline {
 
         subsystems.drivetrain.mechanumDrive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
 //        subsystems.lift.test(-gamepad2.left_stick_y, gamepad2.right_stick_x, gamepad2.a, gamepad2.b);
-        subsystems.lift.basicRun(gamepad2.x, gamepad2.y, gamepad2.a, -gamepad2.left_stick_y, gamepad2.right_stick_x, gamepad2.b, gamepad2.right_bumper, gamepad2.right_bumper);
+        subsystems.lift.runTurretAndArm(
+                gamepad2.x, //shared
+                gamepad2.y, //alliance
+                gamepad2.a, //home
+                -gamepad2.left_stick_y, //lift manual
+                gamepad2.right_stick_x, //turret manual
+                gamepad2.b, //linkage
+                gamepad2.right_bumper, //release
+                gamepad2.right_trigger > 0, //linkage adjust out
+                gamepad2.left_trigger >0, //linkage adjust in
+                gamepad2.left_bumper //alliance side flip
+        );
 
-        subsystems.cap.run(gamepad2.right_trigger > 0);
+        subsystems.cap.run(gamepad2.dpad_left);
         subsystems.intake.run(gamepad2.dpad_down, gamepad2.x || gamepad2.y || gamepad2.left_bumper, subsystems.lift.isReset());
 
         if(gamepad1.dpad_up) {
