a6fecd3097b27e33f7b7287a9c8816ec825d9a02
Soham Patil <soham1053@gmail.com>
2022-03-03 16:23:20 -0800

    clean lift code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
@@ -38,18 +38,17 @@ public class Lift extends Subsystem {
     private boolean linkageOpen = false;
     private final double LINKAGE_MIN_POS = 0.1;
     private final double LINKAGE_MAX_POS = 0.9;
-    private final double LINKAGE_STICK_COEF = 0.0007;
+//    private final double LINKAGE_STICK_COEF = 0.0007;
     private final double ANALOG_ENCODER_VOLTAGE_OFFSET = 0.525;
     private final double TURRET_TICKS_PER_DEGREE = 1456.0 / 360.0;
     private final double TURRET_VOLTS_PER_DEGREE = (3.3 * 5.0) / 360.0;
     private final double TURRET_TICKS_PER_VOLT = TURRET_TICKS_PER_DEGREE / TURRET_VOLTS_PER_DEGREE;
     private double turretOffset;
-//    private final double
 
     private boolean releaseOpen = false;
     private boolean releaseButton = false;
-    private double releasePosOpen = 0.5;
-    private double releasePosClosed = 0.05;
+    private double releasePosOpen = 0.23;
+    private double releasePosClosed = 0.4;
 
     SimplePID turretPID = new SimplePID(0.05,0.0,0.0,-1.0,1.0);
     private AnalogInput turretAngleAnalog; //Temporary until analog input
@@ -91,99 +90,6 @@ public class Lift extends Subsystem {
 
     }
 
-//    public void run(double liftStick, double turretStick, double linkageStick, boolean turretButton, boolean resetButton, boolean releaseButton) {
-//        // Get Positions!
-//        int liftPos = lift.getCurrentPosition();
-//
-//        //have to call AnalogEncoder.update() every loop
-//        turretAngleAnalog.update();
-//        //returns the number of rotations reported by the encoder
-//        double turretRotations = turretAngleAnalog.getCurrentPosition(AnalogEncoder.Mode.INCREMENTAL);
-//        double turretAngle = turretAngleAnalog.getVoltage();
-//
-//        //TODO linkage kinematics
-//
-//        //TODO Convert Analog Encoder Output to Angle(Radians)
-//
-//        //Lift
-//        double liftPower;
-////        double feedForwardCoeff = liftPos > 50 ? 0.15 : 0.0;
-//        if (liftStick != 0) {
-//            //Sigmoid Motor Limits
-//            if (liftStick > 0) {
-//                liftPower = liftStick * sigmoid(LIFT_CONVERGENCE_SPEED * (liftPos - (LIFT_MAX_POS - LIFT_LIMIT_RANGE)));
-//            } else {
-//                liftPower = liftStick * sigmoid(LIFT_CONVERGENCE_SPEED * (LIFT_LIMIT_RANGE / 2 - liftPos));x
-//            }
-//        } else {
-//            if (!liftHolding) {
-//                liftTargetPos = Range.clip(liftPos, LIFT_MIN.value(turretAngle), LIFT_MAX_POS);
-//                liftHolding = true;
-//            }
-//            liftPower = liftPID.run(liftTargetPos, liftPos);
-//        }
-//        lift.setPower(liftPower);
-//
-//        //Turret
-//        //have to call AnalogEncoder.update() every loop
-////        turretAngleAnalog.update();
-//        //returns the number of rotations reported by the encoder
-//        double turretRotations = 0;
-//        double turretAngle = turretAngleAnalog.getVoltage();
-//
-//        double turretPower;
-//        if (turretStick != 0) {
-//            if (turretHolding) {
-//                turretPID.reset();
-//                turretHolding = false;
-//            }
-//            turretPower = turretStick;
-//        } else {
-//            if (!turretHolding) {
-//                turretTargetPos = turretRotations;
-//                turretHolding = true;
-//            }
-//            turretPower = turretPID.run(turretTargetPos, turretRotations);
-//        }
-////        should we put autoAim in the autopath class?
-////        if (autoAim) { //Auto-aim for auto
-////            Point target = new Point(GOAL_X, GOAL_Y);
-////            double angleToTarget = Math.atan2(target.x - Robot.getRobotPose().getX(), target.y - Robot.getRobotPose().getY());
-////            double currentAngle = Robot.getRobotPose().getHeading() + turretAngle;
-////            turretPower = turretPID.run(angleToTarget, currentAngle);
-////        }
-//        turret.setPower(turretStick);
-//
-//        //Linkage
-//        double elapsed = linkageTimer.milliseconds();
-//        double targetPos = linkageOne.getPosition() + LINKAGE_STICK_COEF * elapsed * linkageStick;
-//        linkageOne.setPosition(Range.clip(targetPos, LINKAGE_MIN_POS, LINKAGE_MAX_POS));
-//        linkageTimer.reset();
-//
-//
-//        //Hopper
-//        if (releaseButton && !this.releaseButton) {
-//            this.releaseButton = true;
-//            releaseOpen = !releaseOpen;
-//        } else if (!releaseButton && this.releaseButton) {
-//            this.releaseButton = false;
-//        }
-//
-//        if (releaseOpen) {
-//            release.setPosition(releasePosOpen);
-//        } else {
-//            release.setPosition(releasePosClosed);
-//        }
-//
-//
-//        //Run To Position
-//        if (resetButton) {
-//            linkageOne.setPosition(BASE_LINKAGE_POS);
-//            liftToPosition(BASE_LIFT_POS);
-//            turretToPosition(BASE_TURRET_POS);
-//        }
-//    }
-
     LiftState liftState = LiftState.HOME;
     boolean liftRunning = false;
     boolean turretRunning = false;
@@ -224,9 +130,9 @@ public class Lift extends Subsystem {
         }
 
         if (releaseOpen) {
-            release.setPosition(0.23);
+            release.setPosition(releasePosOpen);
         } else {
-            release.setPosition(0.4);
+            release.setPosition(releasePosClosed);
         }
 
         LiftState prevLiftState = liftState;
@@ -238,7 +144,7 @@ public class Lift extends Subsystem {
             liftState = LiftState.HOME;
         }
         if(liftAdjust != 0 || turretAdjust != 0) {
-           liftState = LiftState.MANUAL;
+            liftState = LiftState.MANUAL;
         }
         
         if(prevLiftState != liftState) {
@@ -300,7 +206,6 @@ public class Lift extends Subsystem {
                     minPos = LIFT_CLEARED_POS;
                 }
 
-                //                  effective deadzone
                 if (Math.abs(liftAdjust) > 0.05) {
                     if (liftHolding) {
                         liftHolding = false;
@@ -324,7 +229,6 @@ public class Lift extends Subsystem {
                         liftTargetPos = Range.clip(liftPos, minPos, LIFT_MAX_POS);
                         liftHolding = true;
                     }
-//                    liftPower = liftAdjust;
                     double feedForward = 0.03;
                     liftPower = liftPID.run(liftTargetPos, liftPos) + feedForward;
                 }
@@ -347,11 +251,7 @@ public class Lift extends Subsystem {
             linkageTwo.setPosition(LINKAGE_MIN_POS);
         }
 
-        if(Math.abs(turretPos) < 30 && Math.abs(liftPos) < 20) {
-            liftReady = true;
-        } else {
-            liftReady = false;
-        }
+        liftReady = Math.abs(turretPos) < 30 && Math.abs(liftPos) < 20;
 
         telemetry.addData("lift state", liftState.toString());
         telemetry.addData("analog encoder", turretAngleAnalog.getVoltage());
